7b36c29c920b97deb15930259bb83e1721896c28
Kevin <weiyih@sheridancollege.ca>
2021-03-09 16:30:51 -0500

    network - convert to coroutines

--- app/src/main/java/com/kevinwei/vote/network/ElectionsApiService.kt
+++ app/src/main/java/com/kevinwei/vote/network/ElectionsApiService.kt
@@ -1,16 +1,23 @@
 package com.kevinwei.vote.network
 
+import com.kevinwei.vote.model.Election
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 // TODO - Replace URL
 private const val BASE_URL = "http://localhost:8080"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -21,8 +28,7 @@ interface ElectionsApiService {
             Call<String>
 
     @GET("elections")
-    fun getElections():
-            Call<String>
+    suspend fun getElections(): List<Election>
 
     @GET("ballots")
     fun getBallots():
