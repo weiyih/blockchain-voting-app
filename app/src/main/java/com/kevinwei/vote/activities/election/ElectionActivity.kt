338c16349301d1f983d8aca203f7bbb0497db07b
Kevin <weiyih@sheridancollege.ca>
2021-03-09 16:53:27 -0500

    renamed activity_main -> activity_election updated view binding

--- app/src/main/java/com/kevinwei/vote/activities/election/ElectionActivity.kt
+++ app/src/main/java/com/kevinwei/vote/activities/election/ElectionActivity.kt
@@ -3,23 +3,19 @@ package com.kevinwei.vote.activities.election
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kevinwei.vote.adapter.ElectionAdapter
-import com.kevinwei.vote.databinding.ActivityMainBinding
+import com.kevinwei.vote.databinding.ActivityElectionBinding
 
 class ElectionActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityElectionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityElectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Instantiate and bind adapter to data
         val electionAdapter = ElectionAdapter()
         binding.electionList.adapter = electionAdapter
     }
-
-
-
-
 }
