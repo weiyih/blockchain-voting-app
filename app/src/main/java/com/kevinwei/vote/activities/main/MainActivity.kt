28c3bb174d1e1e66460babfa66f9734f286f37b7
Kevin <weiyih@sheridancollege.ca>
2021-03-09 13:38:29 -0500

    wip - election main recycler view

--- app/src/main/java/com/kevinwei/vote/activities/main/MainActivity.kt
+++ app/src/main/java/com/kevinwei/vote/activities/main/MainActivity.kt
@@ -3,10 +3,25 @@ package com.kevinwei.vote.activities.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kevinwei.vote.R
+import com.kevinwei.vote.adapter.ElectionAdapter
+import com.kevinwei.vote.databinding.ActivityLoginBinding
+import com.kevinwei.vote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Instantiate and bind adapter to data
+        val electionAdapter = ElectionAdapter()
+        binding.electionList.adapter = electionAdapter
     }
+
+
+
+
 }
