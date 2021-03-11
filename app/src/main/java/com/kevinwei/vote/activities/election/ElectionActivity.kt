dba50fff361e88ffe4f2a6d8df534d41123201a7
Kevin <weiyih@sheridancollege.ca>
2021-03-11 17:08:34 -0500

    dummy test list

--- app/src/main/java/com/kevinwei/vote/activities/election/ElectionActivity.kt
+++ app/src/main/java/com/kevinwei/vote/activities/election/ElectionActivity.kt
@@ -18,28 +18,32 @@ class ElectionActivity : AppCompatActivity() {
 
     private val electionViewModel by viewModels<ElectionViewModel>()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityElectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // RecyclerView
+        setupElectionList()
+    }
+
+    private fun setupElectionList() {
         val electionAdapter = ElectionAdapter()
         binding.electionList.adapter = electionAdapter
 
-        val electionListObserver = Observer<List<Election>> {
-            electionAdapter.data = it
-        }
-        electionViewModel.electionData.observe(this, electionListObserver)
+        electionAdapter.data = testList
 
-
+//        val electionListObserver = Observer<List<Election>> {
+//            electionAdapter.data = it
+//        }
+//        electionViewModel.electionData.observe(this, electionListObserver)
     }
 
-//    private fun setupElectionCards() {
-//        try{
-//            electionViewModel.getElections()
-//        } catch {
-//
-//        }
-//    }
+
+
+    val testList = listOf(
+            Election("1111", "Election A", "Description of Election A", "startDate", "endDate","advStartDate","advEndDate", true,"created","updated"),
+            Election("2222", "Election B", "Description of Election B", "startDate", "endDate","advStartDate","advEndDate", true,"created","updated"),
+    )
+
 }
