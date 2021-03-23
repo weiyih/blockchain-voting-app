230ff6f8101bbbf60ca1fda31f7e98b603a18c63
Kevin <weiyih@sheridancollege.ca>
2021-03-23 00:43:55 -0400

    WIP - Setttings enabling biometric hardware

--- app/src/main/java/com/kevinwei/vote/activities/settings/SettingsFragment.kt
+++ app/src/main/java/com/kevinwei/vote/activities/settings/SettingsFragment.kt
@@ -1,16 +1,86 @@
 package com.kevinwei.vote.activities.settings
 
+import android.annotation.TargetApi
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.biometric.BiometricManager
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import com.kevinwei.vote.AUTHORIZED_BIOMETRICS
 import com.kevinwei.vote.R
 
+
 class SettingsFragment : PreferenceFragmentCompat() {
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        TODO("Not yet implemented")
+        // Load the preferences from an XML resource
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
 
+        setupEnableBiometrics()
+    }
 
+    private fun setupEnableBiometrics() {
+        val currentBiometric = sharedPreferences.getBoolean("biometric", false)
+
+        //Register listener
+        val biometricListener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
+            val valueChanged = preferenceManager.sharedPreferences.getBoolean("biometric", false)
+            when (valueChanged) {
+                true -> showBiometricPrompt()
+                // TODO("false - show dialog warning")
+//                false ->
+            }
+        }
     }
 
 
+    private fun showBiometricPrompt() {
+        val biometricManager = BiometricManager.from(this.requireContext())
+
+        when (biometricManager.canAuthenticate(AUTHORIZED_BIOMETRICS)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Log.e("MY_APP_TAG", "No biometric features available on this device.")
+            // TODO("Implement behaviour")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
+            // TODO("Implement behavkour")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                when (Build.VERSION.SDK_INT) {
+                    Build.VERSION_CODES.R -> launchBiometricEnroll()
+                    Build.VERSION_CODES.P, Build.VERSION_CODES.Q -> launchFingerprintEnroll()
+                }
+            }
+        }
+    }
+
+    @TargetApi(30)
+    private fun launchBiometricEnroll() {
+
+        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                AUTHORIZED_BIOMETRICS
+            )
+        }
+
+    }
+
+    @TargetApi(28)
+    private fun launchFingerprintEnroll() {
+        val enrollIntent = Intent(Settings.ACTION_FINGERPRINT_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                AUTHORIZED_BIOMETRICS
+            )
+        }
+        startActivity(enrollIntent)
+    }
 }
--- app/src/main/java/com/kevinwei/vote/security/Constants.kt
+++ app/src/main/java/com/kevinwei/vote/security/Constants.kt
@@ -1,4 +1,8 @@
 package com.kevinwei.vote
 
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+
 const val SHARED_PREFS_FILENAME = "biometric_prefs"
 const val CIPHERTEXT_WRAPPER = "ciphertext_wrapper"
+const val AUTHORIZED_BIOMETRICS = BIOMETRIC_STRONG or DEVICE_CREDENTIAL
