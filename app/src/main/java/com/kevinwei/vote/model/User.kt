6294a8ff659545e44d849a72039d6ca7253bce78
Kevin <weiyih@sheridancollege.ca>
2021-04-18 15:58:16 -0400

    fixed login user model

--- app/src/main/java/com/kevinwei/vote/model/User.kt
+++ app/src/main/java/com/kevinwei/vote/model/User.kt
@@ -4,16 +4,13 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-object User {
-    @Json( name="username")
-    var username: String? = null
+data class User (
+    @Json( name="email")
+    var username: String? = null,
 
-    @Json (name="user_id")
-    var userId: String? = null
+    @Json(name= "token")
+    var token: String? = null,
 
-//    @Json(name= "token")
-//    var token: String? = null
-
-    @Json (name="verified_status")
-    var verified: Boolean = false
-}
+    @Json (name="verified")
+    var verified: Boolean = false,
+)
--- app/src/test/java/com/kevinwei/vote/UtilsTest.kt
+++ app/src/test/java/com/kevinwei/vote/UtilsTest.kt
@@ -7,8 +7,9 @@ class UtilsTest {
 
     @Test
     fun convertDateToLocalFormatted_ReturnsString() {
-        val testString = "2021-03-01T00:00:00.000Z"
+        val testString = "2021-03-01T00:00:00.000Z-500"
         val expectedString = "Mar. 1, 2021, 12:00:00 a.m."
+
 
         val result = convertDateToLocalFormatted(testString)
 
