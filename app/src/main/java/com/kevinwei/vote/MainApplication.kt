8cee5f7d181d8e92fbfb14631b3809e15b9e16da
Kevin <weiyih@sheridancollege.ca>
2021-04-18 15:59:41 -0400

    hacky method of accessing applicationcontext

---
+++ app/src/main/java/com/kevinwei/vote/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.kevinwei.vote
+
+import android.app.Application
+import android.content.Context
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+    }
+
+    companion object {
+        lateinit var appContext: Context
+    }
+}
