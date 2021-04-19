399551d6f252507964f52b739b143238e6aec37f
Kevin <weiyih@sheridancollege.ca>
2021-04-19 15:02:02 -0400

    deviceId

--- app/src/main/java/com/kevinwei/vote/MainApplication.kt
+++ app/src/main/java/com/kevinwei/vote/MainApplication.kt
@@ -2,15 +2,18 @@ package com.kevinwei.vote
 
 import android.app.Application
 import android.content.Context
+import android.provider.Settings
 
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+        deviceId = Settings.Secure.getString(contentResolver,Settings.Secure.ANDROID_ID);
     }
 
     companion object {
         lateinit var appContext: Context
+        lateinit var deviceId: String
     }
 }
