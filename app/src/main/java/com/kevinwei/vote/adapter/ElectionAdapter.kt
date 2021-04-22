eb140de209b81efb08658f69d52c30849234bb26
Kevin <weiyih@sheridancollege.ca>
2021-04-22 15:14:52 -0400

    made the election card more informative

--- app/src/main/java/com/kevinwei/vote/adapter/ElectionAdapter.kt
+++ app/src/main/java/com/kevinwei/vote/adapter/ElectionAdapter.kt
@@ -5,23 +5,79 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.kevinwei.vote.R
 import com.kevinwei.vote.databinding.CardElectionBinding
 import com.kevinwei.vote.model.Election
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ElectionAdapter(private val clickListener: ElectionVoteListener) :
     ListAdapter<Election, ElectionAdapter.ViewHolder>(ElectionDiffCallback()) {
 
     // Bind UI
-
     class ViewHolder private constructor(val binding: CardElectionBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
 
         // TODO - Handle display logic and function of advanced polling
         fun bind(item: Election, clickListener: ElectionVoteListener) {
             binding.election = item
+
+            // Populate the election dates
+            var electionStart: String = ""
+            var electionEnd: String = ""
+            when (item.advancedPolling) {
+                true -> {
+                    electionStart = convertTimestamp(item.advancedStartDate.toString())
+                    electionEnd = convertTimestamp(item.advancedEndDate.toString())
+                }
+                false -> {
+                    electionStart = convertTimestamp(item.electionStartDate)
+                    electionEnd = convertTimestamp(item.electionEndDate)
+                }
+            }
+
+            val displayDateString = "Starts: $electionStart  Ends: $electionEnd"
+            binding.electionDates.text = displayDateString
+
             binding.clickListener = clickListener
+
+            when (item.voteStatus) {
+                in 0L..1L -> {
+                    binding.voteButton.setText(R.string.vote_now)
+
+                    binding.voteButton.isEnabled = checkValidTime(item)
+                }
+                else -> binding.voteButton.setText(R.string.view_receipt)
+            }
+
+
             binding.executePendingBindings()
         }
+
+        private fun checkValidTime(election: Election): Boolean {
+            val current = Instant.now()
+            var instantStart: Instant
+            if (election.advancedPolling) {
+                instantStart = Instant.parse(election.advancedStartDate)
+            } else {
+                instantStart = Instant.parse(election.electionStartDate)
+            }
+
+            var instantEnd: Instant
+            if (election.advancedPolling) {
+                instantEnd = Instant.parse(election.advancedEndDate)
+            } else {
+                instantEnd = Instant.parse(election.electionEndDate)
+            }
+
+            if (current.isBefore(instantStart)) return false
+            if (current.isAfter(instantEnd)) return false
+            return true
+        }
+
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
@@ -29,6 +85,18 @@ class ElectionAdapter(private val clickListener: ElectionVoteListener) :
                 val binding = CardElectionBinding.inflate(layoutInflater, parent, false)
                 return ViewHolder(binding)
             }
+        }
+
+        private fun convertTimestamp(timestamp: String): String {
+
+            // Note: Timezone may cause issues here on display
+            val instant: Instant = Instant.parse(timestamp)
+            val formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy")
+                .withLocale(Locale.CANADA)
+                .withZone(ZoneId.systemDefault())
+
+            val timeString: String = formatter.format(instant)
+            return timeString
         }
     }
 
@@ -44,7 +112,7 @@ class ElectionAdapter(private val clickListener: ElectionVoteListener) :
     }
 }
 
-class ElectionDiffCallback: DiffUtil.ItemCallback<Election>() {
+class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
     override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
         return oldItem.electionId == newItem.electionId
     }
