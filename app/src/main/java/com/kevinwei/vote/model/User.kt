327d76f9d42f1b925eac5d957531464a4065fcbb
Kevin <weiyih@sheridancollege.ca>
2021-04-21 21:25:19 -0400

    minor change to User model

--- app/src/main/java/com/kevinwei/vote/model/User.kt
+++ app/src/main/java/com/kevinwei/vote/model/User.kt
@@ -1,35 +1,16 @@
 package com.kevinwei.vote.model
 
-import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class User (
     @Json( name="email")
-    var username: String? = null,
+    var username: String = "",
 
     @Json(name= "token")
-    var token: String? = null,
+    var token: String = "",
 
     @Json (name="verified")
-    var verified: Boolean? = false,
-
-    @Json( name= "error")
-    var error: String? = null
+    var verified: Boolean = false,
 )
-//@JsonClass(generateAdapter = true)
-//data class User (
-//    @SerializedName("username")
-//    var username: String? = null,
-//
-//    @SerializedName("token")
-//    var token: String? = null,
-//
-//    @SerializedName("verified")
-//    var verified: Boolean = false,
-//
-//    @SerializedName("verified")
-//    var verified: Boolean = false,
-//
-//)
