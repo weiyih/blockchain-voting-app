1a16747ba579e28d4fedefb7aec101ddebcdef7d
Kevin <weiyih@sheridancollege.ca>
2021-04-26 22:11:43 -0400

    fixed to server url

--- app/src/main/java/com/kevinwei/vote/security/Constants.kt
+++ app/src/main/java/com/kevinwei/vote/security/Constants.kt
@@ -6,6 +6,6 @@ import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 const val SHARED_PREF_FILENAME = "shared_pref"
 const val CIPHERTEXT_WRAPPER = "ciphertext_wrapper"
 const val AUTHORIZED_BIOMETRICS = BIOMETRIC_STRONG
-const val BASE_URL = "http://10.0.2.2/"
+//const val BASE_URL = "http://10.0.2.2/"
 //const val BASE_URL = "http://35.196.56.198/"
-//const val BASE_URL = "http://kevinwei.ca/"
+const val BASE_URL = "http://kevinwei.ca/"
